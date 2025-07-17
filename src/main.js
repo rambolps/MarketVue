6fe412fb0f9b5392bab0a7aae701ebb3b326ac91
Ryan Rambali <ryanrambali@gmail.com>
2025-07-17 01:02:14 -0400

    added primevue and got rid of bloat

--- src/main.js
+++ src/main.js
@@ -2,13 +2,58 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
+
+const DarkPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}',
+    },
+    surface: {
+      0: '#ffffff',
+      50: '{zinc.50}',
+      100: '{zinc.100}',
+      200: '{zinc.200}',
+      300: '{zinc.300}',
+      400: '{zinc.400}',
+      500: '{zinc.500}',
+      600: '{zinc.600}',
+      700: '{zinc.700}',
+      800: '{zinc.800}',
+      900: '{zinc.900}',
+      950: '{zinc.950}',
+    },
+  },
+})
+
+app.use(PrimeVue, {
+  ripple: true,
+  theme: {
+    preset: DarkPreset,
+    options: {
+      dark: true,
+    },
+  },
+})
 
 app.mount('#app')
